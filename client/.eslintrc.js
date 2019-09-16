ed426164b804269f5e1ec02606d8dbd0bbd3c3ce
Dev <ddevamzn@abebooks.com>
2019-09-16 14:42:18 -0700

    Updated Eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
-    // '@vue/standard'
+    '@vue/standard'
   ],
 
   // required to lint *.vue files
