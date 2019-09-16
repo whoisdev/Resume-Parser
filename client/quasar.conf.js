84c4d10c99390fef83fb2513dad46e720b5ae3be
Dev <ddevamzn@abebooks.com>
2019-09-16 11:09:19 -0700

    Updated quasar proxy to point to server

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -79,10 +79,17 @@ module.exports = function (ctx) {
         })
       }
     },
-
     devServer: {
-      // https: true,
-      // port: 8080,
+      proxy: {
+        // proxy all requests starting with /api to jsonplaceholder
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          }
+        }
+      },
       open: true // opens browser window automatically
     },
 
